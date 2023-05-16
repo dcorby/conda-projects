ac276b53f1e5d2d98b677b767ce6de4cc2684ec1
Your Name <you@example.com>
2023-05-15 21:27:29 -0400

    Some formatting

--- clothes-classification/prog.py
+++ clothes-classification/prog.py
@@ -12,8 +12,8 @@ print(tf.__version__)
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+               "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
 if IMAGES:
     plt.figure()
@@ -39,18 +39,19 @@ if IMAGES:
 
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(128, activation="relu"),
     tf.keras.layers.Dense(10)
 ])
 
-model.compile(optimizer='adam',
+model.compile(optimizer="adam",
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+              metrics=["accuracy"])
 
 model.fit(train_images, train_labels, epochs=10)
 
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
-print('\nTest accuracy:', test_acc)
+print("\nTest accuracy:", test_acc)
 
 probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 predictions = probability_model.predict(test_images)
+print(predictions)
