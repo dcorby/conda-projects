4109ce0752b46d4ded022a7da1224f8b939d5408
Your Name <you@example.com>
2023-05-15 21:24:18 -0400

    Working on classification

---
+++ clothes-classification/prog.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+import sys
+IMAGES = True if len(sys.argv) > 1 and sys.argv[1] == "images" else False
+
+print(tf.__version__)
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+if IMAGES:
+    plt.figure()
+    plt.imshow(train_images[0])
+    plt.colorbar()  # viridis colormap, by default
+    plt.grid(False)
+    plt.show()
+
+# You can do this to numpy.ndarray
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+if IMAGES:
+    plt.figure(figsize=(10,10))  # figsize=width & height in inches
+    for i in range(25):
+        plt.subplot(5,5,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[train_labels[i]])
+    plt.show()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
+
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+predictions = probability_model.predict(test_images)
